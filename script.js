9f8c5e65156d1daa216e04e76555b0fd3d5c9839
MichelangeloLeoni <leonimichelangelo17@gmail.com>
2026-08-24 21:50:36 +0200

    Refactor navigation links for mobile compatibility and enhance menu functionality

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
+const nav = document.querySelector("nav");
 const menuIcon = document.getElementById("menuIcon");
 const navLinks = document.getElementById("navLinks");
 const openIcon = document.getElementById("openIcon");
 const closeIcon = document.getElementById("closeIcon");
+const logo = document.querySelector(".logo");
+const mobileLinks = document.querySelectorAll(".mobileLink");
 
-menuIcon.addEventListener("click", () => {
+function closeMenu() {
+    navLinks.classList.remove("active");
+    openIcon.classList.remove("active");
+    closeIcon.classList.remove("active");
+}
+
+function toggleMenu() {
     navLinks.classList.toggle("active");
     openIcon.classList.toggle("active");
     closeIcon.classList.toggle("active");
-})
+}
+
+menuIcon.addEventListener("click", toggleMenu);
+
+logo.addEventListener("click", closeMenu);
+
+mobileLinks.forEach(link => {
+    link.addEventListener("click", closeMenu);
+});
+
+document.addEventListener("click", (event) => {
+    if (!nav.contains(event.target) && navLinks.classList.contains("active")) {
+        closeMenu();
+    }
+});
